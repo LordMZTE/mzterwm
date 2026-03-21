16c851179a4772db022bb38e341a27e5a50a61e5
LordMZTE <lord@mzte.de>
2026-03-21 16:12:25 +0100

    chore(nix): don't make overlay output architecture specific

--- flake.nix
+++ flake.nix
@@ -10,23 +10,25 @@
     { self
     , nixpkgs
     , utils
-    }: utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs { inherit system; };
-      mzterwm = pkgs.callPackage ./package.nix { };
-    in
-    {
-      packages.default = mzterwm;
-      packages.mzterwm = mzterwm;
-      devShells.default = pkgs.mkShell {
-        buildInputs = mzterwm.buildInputs ++ (with pkgs; [
-          zig_0_15
-          pkg-config
-        ]);
-      };
+    }: utils.lib.eachDefaultSystem
+      (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        mzterwm = pkgs.callPackage ./package.nix { };
+      in
+      {
+        packages.default = mzterwm;
+        packages.mzterwm = mzterwm;
+        devShells.default = pkgs.mkShell {
+          buildInputs = mzterwm.buildInputs ++ (with pkgs; [
+            zig_0_15
+            pkg-config
+          ]);
+        };
+      }) // {
       overlays.default = final: prev: {
         mzterwm = final.callPackage ./package.nix { };
       };
-    });
+    };
 }
 
